de2a0eea7dba93da4ee9a1318cf01cc51d6daeb9
Wilian Cesar <wilcesar@live.com>
2021-09-22 21:57:39 -0300

    Criação do projeto
    Criação do projeto em Spring Boot

---
+++ API-RESTful/src/main/java/com/razzies/GoldenRaspberryAwardsAPIRESTful/GoldenRaspberryAwardsApiResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.razzies.GoldenRaspberryAwardsAPIRESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenRaspberryAwardsApiResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenRaspberryAwardsApiResTfulApplication.class, args);
+	}
+
+}
---
+++ API-RESTful/src/test/java/com/razzies/GoldenRaspberryAwardsAPIRESTful/GoldenRaspberryAwardsApiResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.razzies.GoldenRaspberryAwardsAPIRESTful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRaspberryAwardsApiResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
